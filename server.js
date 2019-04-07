d0b466ea34a6a4b5175671065139bfb30323bb8e
neroark <ns_ark@hotmail.com>
2019-04-07 12:11:42 +0200

    feat: add routes

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
 const app = express();
 
 // db config
@@ -9,8 +13,13 @@ const db = require("./config/keys").mongoURI;
 // connect to mongodb
 mongoose
   .connect(db)
-  .then(res => console.log(res))
+  .then(res => console.log("ok"))
   .catch(error => console.log(error));
+
+// use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 app.get("/", (req, res) => res.send("hello wolrd"));
 const port = process.env.PORT || 5000;
