761afd72607a841321c267294eb4faafc37bad81
neroark <ns_ark@hotmail.com>
2019-04-07 11:18:28 +0200

    feat: add server port

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
+
+// db config
+const db = require("./config/keys").mongoURI;
+
+// connect to mongodb
+mongoose
+  .connect(db)
+  .then(res => console.log(res))
+  .catch(error => console.log(error));
 
 app.get("/", (req, res) => res.send("hello wolrd"));
 const port = process.env.PORT || 5000;
