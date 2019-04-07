e47f16be3bb01ccc0437342dc0cd1faf475d772a
neroark <ns_ark@hotmail.com>
2019-04-07 15:58:39 +0200

    feat: add users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 // Load User model
 const User = require("../../models/User");
@@ -32,6 +33,17 @@ router.post("/register", (req, res) => {
         email: req.body.email,
         avatar: avatar, // ou juste  avatar   (es6)
         password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
       });
     }
   });
